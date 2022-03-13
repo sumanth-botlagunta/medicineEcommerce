d6a1d988478c5492c0996bf0625a51fd2239c9a6
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-03-13 23:47:30 +0530

    login signup backend not working

--- src/components/orders/Login.js
+++ src/components/orders/Login.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
 import Navbar from "../navbar/Navbar";
-import './index.css'
+import "./index.css";
+
+const url =
+  "https://medimaxauth.herokuapp.com/api/authentication/login";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      message: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = () => {
+    console.log(this.state);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.auth === false) {
+          this.setState({ message: data.token });
+          console.log(">>>login failed");
+        } else {
+          sessionStorage.setItem("ltk", data.token);
+          console.log(">>> login sucessfully");
+          this.props.history.push("/");
+        }
+      });
+  };
+
   render() {
     return (
       <div>
@@ -10,13 +49,15 @@ export default class Login extends Component {
         <form className="container auth">
           <h2 className="text-center">Login</h2>
           <div className="form-group">
-            <label type="email">Email:</label>
+            <label type="username">Enter Username:</label>
             <input
-              type="email"
+              type="username"
               className="form-control"
-              id="email"
-              placeholder="Enter email"
-              name="email"
+              id="username"
+              placeholder="Enter User Name"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group">
@@ -24,12 +65,14 @@ export default class Login extends Component {
             <input
               type="password"
               className="form-control"
-              id="pwd"
+              id="password"
               placeholder="Enter password"
-              name="pwd"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
             />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>
             Submit
           </button>
         </form>
--- src/components/orders/Signup.js
+++ src/components/orders/Signup.js
@@ -1,21 +1,70 @@
 import React, { Component } from "react";
 import Navbar from "../navbar/Navbar";
 
+// const url =
+//   "https://medimaxauth.herokuapp.com/api/authentication/register";
+
+const url =
+  "http://localhost:8000/api/authentication/register";
 export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      username: "",
+      phone: "",
+      name: "",
+      message: "",
+    };
+  }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    }).then(this.props.history.push("/login"));
+    console.log(">>>> register",JSON.stringify(this.state))
+  };
+
   render() {
     return (
       <div>
         <Navbar />
         <form className="container auth">
-        <h2 className="text-center">Register</h2>
+          <h2 className="text-center">Register</h2>
           <div className="form-group">
-            <label type="string">Name:</label>
+            <label type="string">Enter your name:</label>
             <input
               type="name"
               className="form-control"
-              id="mane"
+              id="name"
               placeholder="Enter your Name"
               name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label type="string">Username:</label>
+            <input
+              type="username"
+              className="form-control"
+              id="username"
+              placeholder="Enter User Name"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group">
@@ -26,6 +75,20 @@ export default class Signup extends Component {
               id="email"
               placeholder="Enter email"
               name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label type="phone">PhoneNumber:</label>
+            <input
+              type="phone"
+              className="form-control"
+              id="phone"
+              placeholder="Enter Phone Number:"
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group">
@@ -35,13 +98,37 @@ export default class Signup extends Component {
               className="form-control"
               id="pwd"
               placeholder="Enter password"
-              name="pwd"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
             />
           </div>
-          <button type="submit" className="btn btn-primary">
+          <div className="form-group">
+            <input
+              type="hidden"
+              className="form-control"
+              id="qualification"
+              name="qualification"
+              value="NO"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="hidden"
+              className="form-control"
+              id="user"
+              name="user"
+              value="customer"
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={this.handleSubmit}
+          >
             Create account
           </button>
-          <h4 className="text-center">Register as a doctor</h4> 
+          <h4 className="text-center">Register as a doctor</h4>
         </form>
       </div>
     );
